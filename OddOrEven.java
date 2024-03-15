0c38cfe239236e05cf3ad449a7ffdff144c7f61d
Murali Krishna G <muralikrishnag2001@gmail.com>
2024-03-15 14:17:13 +0530

    Add files via upload

---
+++ OddOrEven.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class OddOrEven {
+    public static void main(String arg[]){
+        Scanner ip = new Scanner(System.in);
+        int a = ip.nextInt();
+        if(a%2==0){
+            System.out.print(a);
+        }
+        else{
+            System.out.print("odd");
+        }
+    }
+}
