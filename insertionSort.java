e0f0036351f4f4ab2ec395efa76fcaf5cca1bca5
Murali Krishna G <muralikrishnag2001@gmail.com>
2024-03-21 10:48:51 +0530

    Add files via upload

---
+++ insertionSort.java
@@ -0,0 +1,19 @@
+package Sorting;
+
+public class insertionSort {
+    public static void main(String[] args) {
+        int arr[] = {1,2,9,8,6,5,4,7};
+        for(int i=0;i<arr.length;i++){
+            int current = arr[i];
+            int j=i-1;
+            while(j>=0 && current<arr[j]){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=current;
+        }
+        for(int i= 0;i<arr.length;i++){
+            System.out.print(arr[i]);
+        }
+    }
+}
