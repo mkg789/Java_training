e6b289ec4a763acdc219afa8d8aaa65d466c85ae
Murali Krishna G <muralikrishnag2001@gmail.com>
2024-03-22 20:35:38 +0530

    Update OddOrEven.java

--- OddOrEven.java
+++ OddOrEven.java
@@ -3,6 +3,7 @@ public class OddOrEven {
     public static void main(String arg[]){
         Scanner ip = new Scanner(System.in);
         int a = ip.nextInt();
+        // checking even condition
         if(a%2==0){
             System.out.print(a);
         }
