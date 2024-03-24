313808669e02f8944a98dde0c794c7f79775e433
Murali Krishna G <muralikrishnag2001@gmail.com>
2024-03-24 07:24:44 +0530

    Update CumulativeLength.java

--- CumulativeLength.java
+++ CumulativeLength.java
@@ -7,12 +7,13 @@ public class CumulativeLength {
         Scanner ip = new Scanner(System.in);
         int size = ip.nextInt();
         String[] name = new String[size];
+        //Reading array input
         for(int i=0;i<size;i++){
             name[i]= ip.next();
         }
-        int len = 0;
+        int len = 0; //Cumulative length
         for(int i=0;i<size;i++){
-            len+=name[i].length();
+            len+=name[i].length(); // adding length of each variable to len
         }
         System.out.println(len);
     }
